cd4b91e7dd140240318b9f9b36a65bed7cc1ea7d
PanchaliAbh <71926429+PanchaliAbh@users.noreply.github.com>
2020-10-19 22:50:37 +0530

    rout

--- EComPr/src/app/app-routing.module.ts
+++ EComPr/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdminnavbarComponent } from './admin/adminnavbar/adminnavbar.component';
 import { LoginComponent } from './login/login/login.component';
 
 const routes: Routes = [
-  { path: '', component: LoginComponent }
+  { path: '', component: LoginComponent },
+  { path: 'AdminnavbarComponent', component: AdminnavbarComponent }
 ];
 
 @NgModule({
--- EComPr/src/app/app.module.ts
+++ EComPr/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+
+import { AdminnavbarComponent } from './admin/adminnavbar/adminnavbar.component';
+import { LoginComponent } from './login/login/login.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminnavbarComponent,
+    LoginComponent
   ],
   imports: [
+    RouterModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
